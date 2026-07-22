31b1051bdc8bf1e3f2acb71c62024f6864738a42
Alexey Kravtsov <alexey.swe@gmail.com>
2026-07-22 15:20:20 +0200

    replace charging state text with ++/-- symbols

--- plugins/battery.sh
+++ plugins/battery.sh
@@ -12,7 +12,7 @@ BATT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep -i 'AC Power')
 
 if [ -n "$CHARGING" ]; then
-  sketchybar --set $NAME label="${BATT}% (charging)"
+  sketchybar --set $NAME label="++ ${BATT}%"
 else
-  sketchybar --set $NAME label="${BATT}%"
+  sketchybar --set $NAME label="-- ${BATT}%"
 fi
